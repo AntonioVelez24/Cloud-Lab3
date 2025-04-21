471099274636ac08c2daaa376fbc561942cc28d1
AntonioVelez24 <fabricio.velez@tecsup.edu.pe>
2025-04-21 17:13:34 -0500

    Card Game Added

--- src/pages/Game2.tsx
+++ src/pages/Game2.tsx
@@ -2,10 +2,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game2() {
     const { unityProvider, sendMessage } = useUnityContext({
-        loaderUrl: "/CardGame.loader.js",
-        dataUrl: "/CardGame.data.unityweb",
-        frameworkUrl: "/CardGame.framework.js.unityweb",
-        codeUrl: "/CardGame.wasm.unityweb",
+        loaderUrl: "/Game2/CardGame2.loader.js",
+        dataUrl: "/Game2/CardGame2.data",
+        frameworkUrl: "/Game2/CardGame2.framework.js",
+        codeUrl: "/Game2/CardGame2.wasm",
     });
 
     function handleSceneRestart() {
@@ -18,9 +18,10 @@ function Game2() {
                 <div className="centered-content">
                     <h1 className="centered-title">Game 2</h1>
                     <Unity unityProvider={unityProvider} className="centered-unity" />
-                <div className="centered-content">
+
+                    <div className="centered-content">
                     <button onClick={handleSceneRestart}>Restart Scene</button>
-                </div>
+                </div>               
                 </div>
                 <div>
                     <h1>Game 2</h1>
