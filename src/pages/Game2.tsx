ad1a362dc842b1e88398e4d02780b87a7658cc80
AntonioVelez24 <fabricio.velez@tecsup.edu.pe>
2025-04-21 15:17:39 -0500

    Game Added

--- src/pages/Game2.tsx
+++ src/pages/Game2.tsx
@@ -2,10 +2,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game2() {
     const { unityProvider, sendMessage } = useUnityContext({
-        loaderUrl: "/Game2/cloud.loader.js",
-        dataUrl: "/Game2/cloud.data",
-        frameworkUrl: "/Game2/cloud.framework.js",
-        codeUrl: "/Game2/cloud.wasm",
+        loaderUrl: "/CardGame.loader.js",
+        dataUrl: "/CardGame.data.unityweb",
+        frameworkUrl: "/CardGame.framework.js.unityweb",
+        codeUrl: "/CardGame.wasm.unityweb",
     });
 
     function handleSceneRestart() {
